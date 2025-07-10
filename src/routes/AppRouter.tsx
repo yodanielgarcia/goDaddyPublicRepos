7440e37c1e78a47597b1448a07a2cdb728682c64
yodanielgarcia <danielfelipegarciaforero@gmail.com>
2025-07-10 09:54:40 -0500

    routes fix gh

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "../pages/HomePage";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import DetailsPage from "../pages/DetailsPage";
+import HomePage from "../pages/HomePage";
 import NotFoundPage from "../pages/NotFoundPage";
-
+// ...
 export default function AppRouter() {
   return (
-    <BrowserRouter basename="/goDaddyPublicRepos">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/detail/:id" element={<DetailsPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
